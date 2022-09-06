d332e33e6d938e438a74e75a12badf325dbbff7f
Gazali <gazali.ahmad@gmail.com>
2022-09-06 09:36:57 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HighlightsComponent } from './Components/highlights/highlights.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    HighlightsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
